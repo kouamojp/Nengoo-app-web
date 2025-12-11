5571e8709bf267f0fc99aca4179306c9744195ce
Kouamojp <kouamojp@gmail.com>
2025-12-11 01:56:48 -0500

    Add GA4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-RG2S4NX7NH"); // Mets ton Tracking ID GA4
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
